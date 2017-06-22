a48f30c5c0b8156b8d27f616ffba7a3ca584b337
dineshvas <dineshkumar9569@gmail.com>
2017-06-22 09:45:13 +0530

    Add files via upload

---
+++ minus.java
@@ -0,0 +1,44 @@
+package guvi;
+
+import java.util.Calendar;
+
+public class minus {
+	
+	    public static void main(String[] args)
+	    {
+	 
+	       
+	        Calendar cal1 = Calendar.getInstance();
+	        cal1.set(2011, 07, 4, 5, 0, 0);
+	 
+	        
+	        Calendar cal2 = Calendar.getInstance();
+	        cal2.set(2011, 07, 4, 6, 1, 1);
+	 
+	        
+	        long milliSec1 = cal1.getTimeInMillis();
+	        long milliSec2 = cal2.getTimeInMillis();
+	 
+	        long timeDifInMilliSec;
+	        if(milliSec1 >= milliSec2)
+	        {
+	            timeDifInMilliSec = milliSec1 - milliSec2;
+	        }
+	        else
+	        {
+	            timeDifInMilliSec = milliSec2 - milliSec1;
+	        }
+	 
+	        long timeDifSeconds = timeDifInMilliSec / 1000;
+	        long timeDifMinutes = timeDifInMilliSec / (60 * 1000);
+	        long timeDifHours = timeDifInMilliSec / (60 * 60 * 1000);
+	        
+	 
+	        System.out.println("Time differences");
+	       
+	        System.out.println(timeDifSeconds + " Seconds");
+	        System.out.println(timeDifMinutes + " Minutes");
+	        System.out.println(timeDifHours + " Hours");
+	       
+	    }
+	}
